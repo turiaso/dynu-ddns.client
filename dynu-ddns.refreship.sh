17601b0b0269619ceba9c931051bf4f3e4bc7929
turiaso <juan.ruizpachon@gmail.com>
2018-09-28 14:29:10 +0200

    Update dynu-ddns.refreship.sh

--- dynu-ddns.refreship.sh
+++ dynu-ddns.refreship.sh
@@ -2,22 +2,14 @@
 # Script to refresh ip on dynu dns service
 
 username="******"
-#randombitsonfire@gmail.com
 password="******"
 password=$(echo -n $password|sha256sum|head -c 64)
-echo username: $username
-echo password: $password
-# alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-echo myip: $myip
-
-#| sha256sum
 
 echo "wget http://api.dynu.com/nic/update?myip="$myip"&username="$username"&password="$password
 wget -t100 -Oout.txt "http://api.dynu.com/nic/update?myip="$myip"&username="$username"&password="$password 
 
 out=$(cat out.txt)
-
 echo $out
 
 rm out.txt
