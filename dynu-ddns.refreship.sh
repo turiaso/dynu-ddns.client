9cda54faecdbb9e321e82bbfb4369056149aef75
Juan Antonio Ruiz Pachon <jaruiz@adglow.com>
2018-09-28 14:37:22 +0200

    added as params script

--- dynu-ddns.refreship.sh
+++ dynu-ddns.refreship.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Script to refresh ip on dynu dns service
 
-username="******"
-password="******"
+username=$1
+password=$2
 password=$(echo -n $password|sha256sum|head -c 64)
 myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
